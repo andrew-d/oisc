0ffd20c093322dbe78df531f939caff888bcde7f
Andrew Dunham <andrew@du.nham.ca>
2013-12-03 20:29:53 -0800

    More comments on test code

--- test/print_hi.asm
+++ test/print_hi.asm
@@ -1,18 +1,23 @@
-; Manually emit mov
+; Manually emit mov so it's clearer what's being modified.
 start:  subleq curr, curr, $+3
 mod:    subleq printme, Z, $+3
         subleq Z, curr, $+3
         subleq Z, Z, $+3
 
+        ; If we just 'read' a 0 (i.e. null), we're done
         jz curr, done
 
+        ; Print output
         output curr
+
+        ; Increment the print address by 1
         addi 1, mod
 
+        ; Next character
         jmp start
 
+        ; Jumping to a negative address will halt the VM
 done:   jmp -1
-
 
 
 addr:           db 0
