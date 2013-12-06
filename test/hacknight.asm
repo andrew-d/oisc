858051f2c5695afb0abb003344b3f00563d5056a
Andrew Dunham <andrew@du.nham.ca>
2013-12-06 10:16:57 -0800

    Obfuscate hash to compare against

--- test/hacknight.asm
+++ test/hacknight.asm
@@ -1,11 +1,30 @@
-start:  input curr
+start:  addi expected, 9 ; 3*3*19*4001*4423
+        muli expected, 19
+        jmp inp
+ten:    db 10
+inp:    input curr
         jle curr, next
         mul hash, mult
         add hash, curr
-        jmp start
+        jmp inp
 
-expected: db 3026088333     ; 'foobar'
-next:   sub hash, expected
+exp_temp: db 40
+expected: db 0     ; 'foobar', value 3026088333
+next:   mul exp_temp, ten
+        mul exp_temp, ten
+        addi exp_temp, 1        ; exp_temp = 4001
+
+        mul expected, exp_temp
+
+        zero exp_temp
+        addi exp_temp, 44
+        mul exp_temp, ten       ; exp_temp = 440
+        mul exp_temp, ten       ; exp_temp = 4400
+        addi exp_temp, 23       ; exp_temp = 4423
+
+        mul expected, exp_temp  ; should be equal to final val
+
+        sub hash, expected
         jnz hash, end
 
 print:  subleq curr, curr, $+3
