48fe18a9674139d4826f09e8992cd377f7eda99d
Andrew Dunham <andrew@du.nham.ca>
2013-12-17 10:57:20 -0800

    Make harder, add comments

--- test/hacknight.asm
+++ test/hacknight.asm
@@ -1,6 +1,8 @@
 ; Password = XZgvSYhh7C
 start:  jmp char_1
 first:  db good_msg
+
+; 1. Super simple comparison.
 char_1: input first
         subi first, 'X'
         jnz first, end
@@ -8,37 +10,39 @@ char_1: input first
 
 cmp_3:  db 206
 
-; Simple compare with 'curr'
+; 2. Simple compare at address 'curr'
 char_2: input curr
         subi curr, 'Z'
         jnz curr, end
 
-; Write location changed
+; 3. Multiply before comparing.
 char_3: input curr
         muli curr, 2
         sub curr, cmp_3             ; 'g' = 103, * 2 = 206
         jnz curr, end
 
-; Simple compare, TODO more
+; 4. Simple compare, TODO more
 char_4: input curr
-        subi curr, 118              ; 'v' = 118
+        subi curr, 'v'              ; 'v' = 118
         jnz curr, end
 
-; Control flow obfuscated
+; 5. Control flow obfuscated
 char_5: jmp ch5_1
+
+    ; 6. Simple compare (embedded in above)
+    char_6: input first
+            subi first, 'Y'
+            jnz curr, end
+            jmp ch_7_8
+
 ch5_2:  subi curr, 'S'
         jnz curr, end
         jmp char_6
+neg_1:  db -1
 ch5_1:  input curr
         jmp ch5_2
 
-; Simple compare
-char_6: input first
-        subi first, 'Y'
-        jnz curr, end
-        jmp ch_7_8
-
-; Compares twice
+; 7. Compares twice
 ctr:    db 9
 ch_7_8: input curr                  ; compare for 'h' twice
         subi curr, 'h'
@@ -46,17 +50,18 @@ ch_7_8: input curr                  ; compare for 'h' twice
         subi ctr, 8
         jg ctr, ch_7_8
 
-; Simple compare, overwrites code
+; 8. Simple compare, overwrites old code
 char_9: input char_6
         subi char_6, '7'
         jnz char_6, end
 
-; Add of negative number, TODO more
+; 9. Add of negative number, TODO more
 char_10: input curr
-        addi curr, -67              ; 'C' = 67
+        add curr, neg_1
+        addi curr, -66              ; 'C' = 67
         jnz curr, end
 
-; Simple compare for EOF
+; 10. Simple compare for EOF
 end_ch: input curr                  ; check for EOF
         jge curr, end
 
